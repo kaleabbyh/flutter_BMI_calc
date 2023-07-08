b86c9458d2dc7665f9210fab9b289ea54505a5ed
Kaleab Bayih <kaleabbyh@gmail.com>
2023-07-08 18:07:02 +0300

    Update MainActivity.java

--- android/app/src/main/java/co/appbrewery/bmicalculator/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/bmicalculator/MainActivity.java
@@ -5,3 +5,12 @@ import io.flutter.embedding.android.FlutterActivity;
 public class MainActivity extends FlutterActivity {
 
 }
+
+
+// package co.appbrewery.bmicalculator;
+
+// import io.flutter.embedding.android.FlutterActivity;
+
+// public class MainActivity extends FlutterActivity {
+
+// }
